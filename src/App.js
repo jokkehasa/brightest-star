a653626e3e65188d302d44330428580bd12d22f1
Jokke Häsä <jokke.hasa@helsinki.fi>
2022-01-17 01:46:08 +0200

    Added viewing direction and removed text centering

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import { useState, useEffect } from 'react';
 
 function App() {
   const [location, setLocation] = useState(null);
-  const [time, setTime] = useState(null); 
+  const [time, setTime] = useState(null);
+  const [direction, setDirection] = useState(["N", "S", "E", "W", "NW", "SW", "SE", "NE", "Z"]);
 
   const updateCoordinates = () => {
     if (navigator.geolocation) {
@@ -30,28 +31,23 @@ function App() {
         >
           Learn React
         </a>
-        <Location />
       </header>
-    </div>
-  );
-}
-
-function Location() {
-  const [location, setLocation] = useState(null);
-
-  if (!location) {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(setLocation);
-    } else {
-      console.log("Geolocation not supported")
-    }
-  }
-
-  return (
-    <div>
-      {location &&
-        <p>Latitude: {location.coords.latitude}<br />Longitude: {location.coords.longitude}</p>
-      }
+      <div>
+        <table>
+          <tr><td>Latitude:</td>
+              <td>{location && location.coords.latitude}</td></tr>
+          <tr><td>Longitude:</td>
+              <td>{location && location.coords.longitude}</td></tr>
+          <tr><td>Time:</td>
+              <td>{time && time.toString()}</td></tr>
+        </table>
+      </div>
+      <button type="button" onClick={updateCoordinates}>
+        Update location and time
+      </button>
+      <div>
+        <p>Viewing direction: {direction.toString()}</p>
+      </div>
     </div>
   );
 }
